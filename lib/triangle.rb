73bcb656a4b63bb0bf988ce970bdb029a264c2d1
David Forer <david.j.forer@gmail.com>
2019-04-11 14:02:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,10 @@
 class Triangle
    attr_accessor :side_1, :side_2, :side_3
+   
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
-    @sides = [@side_1, @side_2, @side_3].sort 
   end 
 
    def kind 
